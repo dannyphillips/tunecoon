3633e75a00e8a74b76b8e1d4232958a1db67505e
Danny Phillips <danny.phillips8@gmail.com>
2020-04-23 01:30:12 -0700

    simple UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ import { Fragment } from 'react'
 import { NextPage, NextPageContext } from "next";
 import { graphql } from "@octokit/graphql"
 import { GitPullRequest } from "@primer/octicons-react";
-import Link from "next/link";
 
 const Home: NextPage<any> = ({ repos }) => {
   debugger;
@@ -18,12 +17,12 @@ const Home: NextPage<any> = ({ repos }) => {
     <Flex flexDirection="column" alignItems="center">
       <Box width={600}>
         <Heading>My Repositories</Heading>
-        <Link href="/about">About</Link>
         <table>
           <thead>
             <tr>
               <th>Name</th>
               <th>Open Pulls</th>
+              <th>Date</th>
             </tr>
           </thead>
 
@@ -31,7 +30,7 @@ const Home: NextPage<any> = ({ repos }) => {
             {repos && repos.user.repositories.nodes.map(r => (
               <Fragment key={r.name}>
                 <tr>
-                  <td>{r.name}</td>
+                  <td><a href={r.url}>{r.name}</a></td>
                   <td>
                     <Flex alignItems="center">
                       <CircleOcticon icon={GitPullRequest} size={16} />
@@ -42,7 +41,8 @@ const Home: NextPage<any> = ({ repos }) => {
                 {r.pullRequests && r.pullRequests.nodes.map((p) => (
                   <tr key={p.title}>
                     <td>{`${p.number}: ${p.title}`}</td>
-                    <td>{p.mergeable}</td>
+                    <td><a href={p.url}>{p.mergeable}</a></td>
+                    <td>{p.createdAt}</td>
                   </tr>
                 ))}
               </Fragment>
@@ -69,6 +69,7 @@ Home.getInitialProps = async (context: NextPageContext) => {
       repositories(first: 50, affiliations: OWNER) {
         nodes {
           name
+          url
           defaultBranchRef {
             ... on Ref {
               associatedPullRequests {
@@ -82,6 +83,7 @@ Home.getInitialProps = async (context: NextPageContext) => {
               mergeable
               createdAt
               number
+              url
             }
             totalCount
           }
