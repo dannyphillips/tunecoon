0b37bc037153401df6a350d29a0986551e4c1d52
Danny Phillips <danny.phillips8@gmail.com>
2020-04-27 22:35:38 -0700

    more styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,9 @@ import {
   Flex,
   Heading,
   Label,
-  StyledOcticon
+  Link,
+  StyledOcticon,
+  Text
 } from "@primer/components";
 import { NextPage, NextPageContext } from "next";
 import { graphql } from "@octokit/graphql"
@@ -17,12 +19,22 @@ import styled from 'styled-components'
 
 const RepoCard = styled(Button)`
   width: 100%;
-  margin: 10px 0px;
+  margin-top: 8px;
 `
-const IconBlock = styled.div`
-  width: 100px;
-  display: inline-block;
+const BranchCard = styled.div`
+  padding: 0px 16px;
+  background-color: #fdfdfd;
+  border: solid #e4e4e4 1px;
+  border-top: 0px;
 `
+const IconBlock = styled(Flex)``
+
+const getStatus = (condition, successIcon, failIcon) =>
+  condition == true ?
+    <StyledOcticon icon={successIcon} size={16} color="green.5" ml={2}/>
+  :
+    <StyledOcticon icon={failIcon} size={16} color="red.5" ml={2}/>;
+
 
 const Home: NextPage<any> = ({ repos }) => {
   return(
@@ -34,70 +46,53 @@ const Home: NextPage<any> = ({ repos }) => {
             <Box>
               <Details key={r.name}>
                 <RepoCard as="summary">
-                    <Flex justifyContent="space-between" alignItems="center">
-                      <div><a href={r.url}>{r.name}</a></div>
-                      <IconBlock>
-
-                        <div>
-                          <Flex alignItems="center">
-                            <CircleOcticon icon={GitPullRequest} size={16} />
-                            <CounterLabel>{r.pullRequests.totalCount}</CounterLabel>
-                          </Flex>
-                        </div>
-                        <div>
-                          {(r.defaultBranchRef != null) ?
-                            (
-                              r.defaultBranchRef.target.status == null ?
-                                <StyledOcticon icon={Question} size={32} color="yellow.5" mr={2} />
-                              :
-                                (
-                                  r.defaultBranchRef.target.status.state == "SUCCESS" ?
-                                    <StyledOcticon icon={Check} size={32} color="green.5" mr={2} />
-                                  :
-                                    <StyledOcticon icon={X} size={32} color="red.5" />
-                                )
-                            )
-                          :
-                            <div>No Targets</div>
-                          }
-                        </div>
-                        <div>
-                          <Label as="summary">{r.pullRequests.nodes.length > 0 && 'Show More'}</Label>
-                        </div>
-                      </IconBlock>
-                    </Flex>
-                </RepoCard>
-                {r.pullRequests && r.pullRequests.nodes.map((p) => (
-                  <div key={p.title}>
-                    <div>{`${p.number}: ${p.title}`}</div>
-                    <div>
-                      <a href={p.url}>
-                        {p.mergeable ?
-                          <StyledOcticon icon={Check} size={32} color="green.5" mr={2} />
-                        :
-                          <StyledOcticon icon={X} size={32} color="red.5" />
-                        }
-                      </a>
-                    </div>
-                    <div>
-                      {p.baseRef != null ?
+                  <Flex justifyContent="space-between" alignItems="center">
+                    <Text><Link href={r.url}>{r.name}</Link></Text>
+                    <IconBlock justifyContent="space-around" alignItems="center">
+                      {r.pullRequests.nodes.length > 0 && <Label outline ml={2}>Show More</Label>}
+                      <Flex alignItems="center" ml={2}>
+                        <CircleOcticon icon={GitPullRequest} size={16} />
+                        <CounterLabel>{r.pullRequests.totalCount}</CounterLabel>
+                      </Flex>
+                      {(r.defaultBranchRef != null) ?
                         (
-                          p.baseRef.target.status == null ?
-                            <StyledOcticon icon={Question} size={32} color="yellow.5" mr={2} />
+                          r.defaultBranchRef.target.status == null ?
+                            // No CI Setup
+                            <StyledOcticon icon={Question} size={16} color="yellow.5" ml={2}/>
                           :
-                            (
-                              p.baseRef.target.status.state == "SUCCESS" ?
-                                <StyledOcticon icon={Check} size={32} color="green.5" mr={2} />
-                              :
-                                <StyledOcticon icon={X} size={32} color="red.5" />
-                            )
+                            // CI is Passing / Failing
+                            getStatus(r.defaultBranchRef.target.status.state == "SUCCESS", Check, X)
                         )
                       :
-                        <div>No Targets</div>
+                        <Label>No Branches</Label>
                       }
-                    </div>
-                    <div>{p.createdAt}</div>
-                  </div>
+                    </IconBlock>
+                  </Flex>
+                </RepoCard>
+                {r.pullRequests && r.pullRequests.nodes.map((p) => (
+                  <BranchCard key={p.title}>
+                    <Flex justifyContent="space-between" alignItems="center">
+                      <Link href={p.url}>
+                        <Text>{`${p.number}: ${p.title}`}</Text>
+                      </Link>
+                      <IconBlock justifyContent="space-around" alignItems="center">
+                        <Text fontStyle="italic" fontSize={10} ml={2}>{p.createdAt}</Text>
+                          {getStatus(p.mergeable, Check, X)}
+                        {p.baseRef != null ?
+                          (
+                            p.baseRef.target.status == null ?
+                              // No CI Setup
+                              <StyledOcticon icon={Question} size={16} color="yellow.5" ml={2}/>
+                            :
+                              // CI is Passing / Failing
+                              getStatus(p.baseRef.target.status.state == "SUCCESS", Check, X)
+                          )
+                        :
+                          <Label>No PRs</Label>
+                        }
+                      </IconBlock>
+                    </Flex>
+                  </BranchCard>
                 ))}
               </Details>
             </Box>
